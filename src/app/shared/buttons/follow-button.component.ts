037c3318e1a42e8db340aa29e36c6222e5558052
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2022-12-16 13:32:30 +0200

    Update follow-button.component.ts

--- src/app/shared/buttons/follow-button.component.ts
+++ src/app/shared/buttons/follow-button.component.ts
@@ -22,17 +22,15 @@ export class FollowButtonComponent {
 
   toggleFollowing() {
     this.isSubmitting = true;
-    // TODO: remove nested subscribes, use mergeMap
 
     this.userService.isAuthenticated.pipe(concatMap(
       (authenticated) => {
-        // Not authenticated? Push to login screen
+ 
         if (!authenticated) {
           this.router.navigateByUrl('/login');
           return of(null);
         }
 
-        // Follow this profile if we aren't already
         if (!this.profile.following) {
           return this.profilesService.follow(this.profile.username)
           .pipe(tap(
@@ -43,7 +41,6 @@ export class FollowButtonComponent {
             err => this.isSubmitting = false
           ));
 
-        // Otherwise, unfollow this profile
         } else {
           return this.profilesService.unfollow(this.profile.username)
           .pipe(tap(
